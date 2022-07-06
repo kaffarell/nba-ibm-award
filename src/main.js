1d2d7b00b460d028ae0dafafed53f543e00ea14f
kaffarell <gabrielgoller123@gmail.com>
2022-07-06 17:58:55 +0200

    :zap: Improved getGamesByTeam request

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ async function getPlayerStats(player_id) {
     let response = await axios.request(options);
     //console.log(response.data);
     let data = response.data.data;
-    spinner_stats.succeed(); 
+    spinner_stats.succeed();
     /*
     data = data.sort((a, b) => {
         let a_date = new Date(a.game.date); 
@@ -69,27 +69,14 @@ async function getPlayerStats(player_id) {
     return playerstats;
 }
 
-async function getAllPlayers(page) {
-    const options = {
-        method: 'GET',
-        url: 'https://free-nba.p.rapidapi.com/players',
-        params: { page: page, per_page: '100'},
-        headers: {
-            'X-RapidAPI-Key': '5ef504cea6msh615586526b620d4p1b13d7jsnad0a11e7f91f',
-            'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-        }
-    };
 
-    let response = await axios.request(options);
-    let data = response.data.data;
-    return data;
-}
+async function getAllGamesByTeam(team_id) {
+    let wins = 0;
 
-async function getAllGames(page) {
     const options = {
         method: 'GET',
         url: 'https://free-nba.p.rapidapi.com/games',
-        params: { page: page, per_page: '100', seasons: [2021]},
+        params: { page: '0', per_page: '100', seasons: [2021], team_ids: [team_id] },
         headers: {
             'X-RapidAPI-Key': '5ef504cea6msh615586526b620d4p1b13d7jsnad0a11e7f91f',
             'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
@@ -97,37 +84,20 @@ async function getAllGames(page) {
     };
 
     let response = await axios.request(options);
+    console.log(response.data);
     let data = response.data.data;
-    return data;
-}
-
-async function getAllGamesFromTeam(team_id) {
-    let game_ids = [];
-    let promises = [];
-    for(let i = 0; i < 15; i++) {
-        promises.push(getAllGames(i));
-    }
-    let values = await Promise.all(promises)
-    let wins = 0;
-    values.map((a) => {
-        a.map((e) => {
-            // Get all the games from a team
-            if(e.home_team.id == team_id || e.visitor_team.id == team_id) {
-                game_ids.push(e.id);
-
-                // count wins
-                if(e.home_team.id == team_id && (e.home_team_score > e.visitor_team_score)){
-                    // home team wins
-                    wins++;
-                }
-                if(e.visitor_team.id == team_id && (e.visitor_team_score > e.home_team_score)){
-                    // visitor team wins
-                    wins++;
-                }
-            }
-        });
+    data.map(game => {
+        // count the wins
+        if (game.home_team.id == team_id && (game.home_team_score > game.visitor_team_score)) {
+            // home team wins
+            wins++;
+        }
+        if (game.visitor_team.id == team_id && (game.visitor_team_score > game.home_team_score)) {
+            // visitor team wins
+            wins++;
+        }
     });
-    return {game_ids, wins};
+    return { data, wins };
 }
 
 async function getSinglePageTeamStat(page, gamesFromTeam) {
@@ -142,13 +112,14 @@ async function getSinglePageTeamStat(page, gamesFromTeam) {
     };
 
     let response = await axios.request(options);
+    console.log(response.data);
     return response.data.data;
 }
 
-async function getTeamStats(team_id) {
 
+async function getTeamStats(team_id) {
     const spinner_games = ora('Getting all games from specific team').start();
-    let {gamesFromTeam, wins} = await getAllGamesFromTeam(team_id);
+    let { gamesFromTeam, wins } = await getAllGamesByTeam(team_id);
     spinner_games.succeed();
 
     // Make a single request to get the amount of pages
@@ -164,12 +135,11 @@ async function getTeamStats(team_id) {
 
     let response = await axios.request(options);
     // Get amount of pages
-
-    const spinner_stats = ora('Getting stats from every game').start();
-
     let pages = response.data.meta.total_pages;
+
+    const spinner_stats = ora(`Getting stats from every game (checking ${response.data.meta.total_count} stats and making ${response.data.meta.total_pages} requests)`).start();
     let promises = [];
-    for(let i = 0; i < pages; i++) {
+    for (let i = 0; i < pages; i++) {
         promises.push(getSinglePageTeamStat(i, gamesFromTeam));
     }
     let allPlayerStats = [];
@@ -178,7 +148,7 @@ async function getTeamStats(team_id) {
     // Get only stats from the wanted team (and not from the opposing team)
     values.map((a) => {
         a.map((e) => {
-            if(e.team.id == team_id){
+            if (e.team.id == team_id) {
                 allPlayerStats.push(e);
             }
         });
@@ -229,13 +199,17 @@ async function getTeamStats(team_id) {
     return teamstats;
 }
 
-let teamStats = await getTeamStats(14)
-let playerStats = await getPlayerStats(237)
+async function calculate() {
+    let teamStats = await getTeamStats(14)
+    let playerStats = await getPlayerStats(237)
 
-const spinner_calculating = ora('Calculating').start();
-let divident = playerStats.pts - playerStats.fga + playerStats.reb + playerStats.ast + playerStats.stl + playerStats.blk - playerStats.pf - playerStats.to + (teamStats.wins * 10) * 250;
-let divisor = teamStats.pts - teamStats.fga + teamStats.reb + teamStats.ast + teamStats.stl + teamStats.blk - teamStats.pf - teamStats.to;
-let result = divident / divisor;
-spinner_calculating.succeed();
+    const spinner_calculating = ora('Calculating').start();
+    let divident = playerStats.pts - playerStats.fga + playerStats.reb + playerStats.ast + playerStats.stl + playerStats.blk - playerStats.pf - playerStats.to + (teamStats.wins * 10) * 250;
+    let divisor = teamStats.pts - teamStats.fga + teamStats.reb + teamStats.ast + teamStats.stl + teamStats.blk - teamStats.pf - teamStats.to;
+    let result = divident / divisor;
+    spinner_calculating.succeed();
 
-console.log('Result = ' + result);
+    console.log('Result = ' + result);
+}
+
+calculate();
